ede0f27aec3eb6f61459467ba22a1c448777494f
Rodrigo José Sandino Salinas <sandinorodrigo@unitec.edu>
2016-06-04 09:26:24 -0600

    Funciones operadores

--- persona.cpp
+++ persona.cpp
@@ -11,13 +11,33 @@ persona::persona(){
 
 persona::persona(string nombre, bool genero, string colorCabello, string colorPiel, string colorOjos, bool fertil):
 				 nombre(nombre),genero(genero),colorCabello(colorCabello),colorPiel(colorPiel),colorOjos(colorOjos),fertil(fertil){
-
 }
 
-bool persona::operator+(const persona& humano){
-	
+const persona& persona::operator+(const persona& humano){
+	if (humano.colorCabello == "oscuro" || humano.colorOjos == "oscuros" || humano.colorPiel == "blanca"){
+		this -> genCabello = "AA";
+		humano.genCabello = "AA";
+		this -> genOjos = "AA";
+		humano.genOjos = "AA";
+		this -> genPiel = "AA";
+		humano.genPiel = "AA";
+	}
+	if (humano.colorCabello == "rubio" || humano.colorOjos == "verdes" || humano.colorPiel == "oscura"){
+		this -> genCabello = "aa";
+		humano.genCabello = "aa";
+		this -> genOjos = "aa";
+		humano.genOjos = "aa";
+		this -> genPiel = "aa";
+		humano.genPiel = "aa";
+	}
+	if (humano.colorCabello == "pelirrojo" || humano.colorOjos == "azules"){
+		this -> genCabello = "bb";
+		humano.genCabello = "bb";
+		this -> genOjos = "bb";
+		humano.genOjos = "bb";
+	}
 }
 
-bool persona::operator*(const persona& humano){
+const persona& persona::operator*(const persona& humano){
 	
 }
